e118665020599291a7368c979229bb335b1d519b
kpracheta <prachetakhadgi@gmail.com>
2023-10-14 17:44:36 +0530

    page transition added

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+// var preloader = document.getElementById('loading');
+//         function loadingfun(){
+//             preloader.style.display = 'none';
+// }
 window.onload = ()=>{
     const transition_el = document.querySelector('.transition');
-
+    const anchors = document.querySelector('a');
     setTimeout(()=>{
-        transition_el.class.List.remove('is-active');
+        transition_el.classList.remove('is-active');
     }, 500);
+    for(let i=0; i<anchors.length; i++){
+const anchor = anchor[i];
+anchor.addEventListener('click', e=>{
+    e.preventDefault();
+    let target = s.target.href;
+
+    transition_el.classList.add('is-active');
+    setTimeout(()=>{
+        window.location.href = target;
+    }, 500)    
+})};
 }
