6f69314168f6301b4f87b7755ac6792698169298
kpracheta <prachetakhadgi@gmail.com>
2023-10-15 23:13:01 +0530

    responsive

--- main.js
+++ main.js
@@ -20,3 +20,12 @@ anchor.addEventListener('click', e=>{
     }, 500)    
 })};
 }
+const toggleBtn = document.querySelector(".toggle_btn")
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
+}
